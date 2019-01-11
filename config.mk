e05d66ab4d38b3003ae653467b3f3186127298e9
Marco Caimi <marco.caimi@fastweb.it>
2019-01-11 11:37:22 +0100

    [+] Minor source fixes, mostly cosmetic.

--- config.mk
+++ config.mk
@@ -2,6 +2,7 @@
 VERSION = 0.8.1
 
 # Customize below to fit your system
+PKG_CONFIG = pkg-config
 
 # paths
 PREFIX = /usr/local
@@ -13,21 +14,22 @@ X11LIB = /usr/X11R6/lib
 # includes and libs
 INCS = -I$(X11INC) \
 			 -I /usr/include \
-       `pkg-config --cflags fontconfig` \
-       `pkg-config --cflags freetype2`
+       `$(PKG_CONFIG) --cflags fontconfig` \
+       `$(PKG_CONFIG) --cflags freetype2`
 
 ifeq ($(shell uname),OpenBSD)
+CC = cc
 OS = OpenBSD
 CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600 -D_BSD_SOURCE
 LIBS = -L$(X11LIB) -lm -lX11 -lutil -lXft \
-       `pkg-config --libs fontconfig` \
-       `pkg-config --libs freetype2`
+       `$(PKG_CONFIG) --libs fontconfig` \
+       `$(PKG_CONFIG) --libs freetype2`
 else
 OS = Linux
 CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
 LIBS = -L/usr/lib -lc -L${X11LIB} -lm -lrt -lX11 -lutil -lXft -lXrender\
-       `pkg-config --libs fontconfig` \
-       `pkg-config --libs freetype2`
+       `$(PKG_CONFIG) --libs fontconfig` \
+       `$(PKG_CONFIG) --libs freetype2`
 endif
 
 # flags
