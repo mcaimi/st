2fe0c9f440ffe12d4471cfccd14d2df4548f1c12
Marco Caimi <caimi.marco@protonmail.ch>
2020-05-13 14:25:00 +0200

    Launch scroll program with the default shell

--- st.c
+++ st.c
@@ -704,9 +704,12 @@ execsh(char *cmd, char **args)
   if (args) {
     prog = args[0];
     arg = NULL;
-  } else if (scroll || utmp) {
-    prog = scroll ? scroll : utmp;
-    arg = scroll ? utmp : NULL;
+  } else if (scroll) {
+    prog = scroll;
+    arg = scroll ? utmp : sh;
+  } else if (utmp) {
+    prog = utmp;
+    arg = NULL;
   } else {
     prog = sh;
     arg = NULL;
