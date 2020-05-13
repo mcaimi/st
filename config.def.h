a9eec3c95bf1f4a80ea7f847a6af2cea0d3ff42b
Marco Caimi <caimi.marco@protonmail.ch>
2020-05-13 12:30:50 +0200

    Upstream patch: Add support for scroll(1)
    Scroll is a program that stores all the lines of its child and be used in st as
    a way of implementing scrollback.
    
    This solution is much better than implementing the scrollback in st itself
    because having a different program allows to use it in any other program
    without doing modifications to those programs.

--- config.def.h
+++ config.def.h
@@ -25,13 +25,14 @@ int debug = 0;
 /*
  * What program is execed by st depends of these precedence rules:
  * 1: program passed with -e
- * 2: utmp option
+ * 2: scroll and/or utmp option
  * 3: SHELL environment variable
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
 char *shell = (char *)"/bin/bash";
 char *utmp = NULL;
+char *scroll = NULL;
 char *stty_args = (char *)"stty raw pass8 nl -echo -iexten -cstopb 38400";
 
 /* identification sequence returned in DA and DECID */
--- st.c
+++ st.c
@@ -686,7 +686,7 @@ die(const char *errstr, ...)
 void
 execsh(char *cmd, char **args)
 {
-  char *sh, *prog;
+  char *sh, *prog, *arg;
   const struct passwd *pw;
 
   errno = 0;
@@ -700,12 +700,17 @@ execsh(char *cmd, char **args)
   if ((sh = getenv("SHELL")) == NULL)
     sh = (pw->pw_shell[0]) ? pw->pw_shell : cmd;
 
-  if (args)
+  if (args) {
     prog = args[0];
-  else if (utmp)
-    prog = utmp;
-  else
+    arg = NULL;
+  } else if (scroll || utmp) {
+    prog = scroll ? scroll : utmp;
+    arg = scroll ? utmp : NULL;
+  } else {
     prog = sh;
+    arg = NULL;
+  }
+
   DEFAULT(args, ((char *[]) {prog, NULL}));
 
   unsetenv("COLUMNS");
