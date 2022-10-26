893a93dc8d07461df0f462918a20f182615a812c
Marco Caimi <mcaimi@redhat.com>
2022-10-26 15:44:48 +0200

    Upstream Patch: fix buffer overflow when handling long composed input
    To reproduce the issue:
    
    "
    If you already have the multi-key enabled on your system, then add this line
    to your ~/.XCompose file:
    
    [...]
    <question> <T> <E> <S> <T> <question> :
    "1234567890123456789012345678901234567890123456789012345678901234567890"
    "
    
    Reported by and an initial patch by Andy Gozas <andy@gozas.me>, thanks!
    
    Adapted the patch, for now st (like dmenu) handles a fixed amount of composed
    characters, or otherwise ignores it. This is done for simplicity sake.
    
    Author: Hiltjo Posthuma <hiltjo@codemadness.org>
    Date:   Tue, 25 Oct 2022 17:11:11 +0200

--- x.c
+++ x.c
@@ -2344,7 +2344,7 @@ void
 kpress(XEvent *ev)
 {
   XKeyEvent *e = &ev->xkey;
-  KeySym ksym;
+  KeySym ksym = NoSymbol;
   char buf[64], *customkey;
   int len;
   Rune c;
@@ -2354,10 +2354,12 @@ kpress(XEvent *ev)
   if (IS_SET(MODE_KBDLOCK))
     return;
 
-  if (xw.ime.xic)
+  if (xw.ime.xic) {
     len = XmbLookupString(xw.ime.xic, e, buf, sizeof buf, &ksym, &status);
-  else
+      if (status == XBufferOverflow) return;
+  } else {
     len = XLookupString(e, buf, sizeof buf, &ksym, NULL);
+  }
 
   /* 1. shortcuts */
   for (bp = shortcuts; bp < shortcuts + LEN(shortcuts); bp++) {
