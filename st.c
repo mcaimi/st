590e7bc388edfffd8029dbcd193c71c09f728133
Marco Caimi <mcaimi@redhat.com>
2024-02-22 11:53:30 +0100

    Upstream patch: csi: check for private marker in 'S' case
    The handler for 'S' final character does not check for a private
    marker. This can cause a conflict with a sequence called 'XTSMGRAPHICS'
    which also has an 'S' final character, but uses the private marker '?'.
    Without checking for a private marker, st will perform a scroll up
    operation when XTSMGRAPHICS is seen, which can cause unexpected display
    artifacts.
    
    Author: Tim Culverhouse <tim@timculverhouse.com>

--- st.c
+++ st.c
@@ -1852,6 +1852,7 @@ csihandle(void)
     }
     break;
   case 'S': /* SU -- Scroll <n> line up */
+    if (csiescseq.priv) break;
     DEFAULT(csiescseq.arg[0], 1);
     tscrollup(term.top, csiescseq.arg[0], 0);
     break;
